bec9a7ad7373ee40e2e33b4a6c9104ef54c05d23
汪浩浩 <whh881114@gmail.com>
2025-04-27 11:25:31 +0800

    自定义inbound-agent镜像：go-1.24.2编译环境。

--- jenkins/inbound-agent/go/build.sh
+++ jenkins/inbound-agent/go/build.sh
@@ -2,7 +2,7 @@
 
 registry_url="harbor.idc.roywong.work"
 registry_project="library"
-image="$registry_url"/"$registry_project"/jenkins/inbound-agent:latest
+image="$registry_url"/"$registry_project"/jenkins/inbound-agent-go:1.24.2
 
 docker build -t $image .
 docker push $image
