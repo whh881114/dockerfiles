de9294a9ee02e6df34fbd2b42359dcda24934a21
汪浩浩 <whh881114@gmail.com>
2025-04-29 14:28:13 +0800

    调整构建

--- jenkins/inbound-agent/maven/build.sh
+++ jenkins/inbound-agent/maven/build.sh
@@ -2,7 +2,19 @@
 
 registry_url="harbor.idc.roywong.work"
 registry_project="library"
-image="$registry_url"/"$registry_project"/jenkins/inbound-agent-maven:3.9.9
 
-docker build -t $image .
+image="$registry_url"/"$registry_project"/jenkins/inbound-agent-maven:jdk8
+docker build -t $image -f Dockerfile-jdk8 .
+docker push $image
+
+image="$registry_url"/"$registry_project"/jenkins/inbound-agent-maven:jdk11
+docker build -t $image -f Dockerfile-jdk11 .
+docker push $image
+
+image="$registry_url"/"$registry_project"/jenkins/inbound-agent-maven:jdk17
+docker build -t $image -f Dockerfile-jdk17 .
+docker push $image
+
+image="$registry_url"/"$registry_project"/jenkins/inbound-agent-maven:jdk21
+docker build -t $image -f Dockerfile-jdk21 .
 docker push $image
