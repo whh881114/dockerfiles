68937494767715773148d0a293dbe088d6b7f761
汪浩浩 <whh881114@gmail.com>
2025-05-14 10:31:48 +0800

    自定义argocd镜像

---
+++ argocd/README.md
@@ -0,0 +1,4 @@
+# 自定义argocd镜像
+
+## 2025-05-14
+- 在原始镜像`quay.io/argoproj/argocd:v2.12.1`的基础上，添加`jsonnet`工具。
---
+++ argocd/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+registry="harbor.idc.roywong.work"
+repository="public"
+image="$registry/$repository/argocd:v2.12.1-2025-05-14"
+
+docker build -t $image .
+docker push $image
