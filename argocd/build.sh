5c65eea5752174727271fa5b823bf860caef9e28
汪浩浩 <whh881114@gmail.com>
2025-05-14 11:19:47 +0800

    argocd：更新镜像。

--- argocd/build.sh
+++ argocd/build.sh
@@ -2,7 +2,7 @@
 
 registry="harbor.idc.roywong.work"
 repository="public"
-image="$registry/$repository/argocd:v2.12.1-2025-05-14"
+image="$registry/$repository/argocd:v2.12.1-2025-05-14-v1.1"
 
 docker build -t $image .
 docker push $image
