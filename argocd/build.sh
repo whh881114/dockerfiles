7d34fe26cb7ec6c01154d33d8db07723407bc81e
汪浩浩 <whh881114@gmail.com>
2025-05-22 13:33:05 +0800

    argocd：使用多层构建，添加curl命令。

--- argocd/build.sh
+++ argocd/build.sh
@@ -2,7 +2,7 @@
 
 registry="harbor.idc.roywong.work"
 repository="public"
-image="$registry/$repository/argocd-cmp-helm:v2.12.1-2025-05-14-v1.3"
+image="$registry/$repository/argocd-cmp:v2.12.1-2025-05-22-v1.0"
 
 docker build -t $image .
 docker push $image
